92f36277c858acc1e98499631373308e2fd1117d
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-02 15:38:15 +0100

    The Section no 2 finished + initial of Section no 3 GUI Interface added

---
+++ Practical_Part/2.Interface_getting_know/1.API_Interface.js
@@ -0,0 +1,4 @@
+// THIS PART IS DEDICATED TO API INTERFACE
+/* API is an interface which allows communication between Back end and Front end part of application
+Simply That Interface allows the communication between different systems
+*/
