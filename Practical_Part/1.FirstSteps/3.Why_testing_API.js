7aea56b9310f39e8764a8cc904a9719a7d7da93c
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-02 14:26:40 +0100

    The part of Advantages and Disadvantages of API Testing description added

---
+++ Practical_Part/1.FirstSteps/3.Why_testing_API.js
@@ -0,0 +1,23 @@
+// THIS PART ANSWERS THE QUESTION WHY SHALL WE TEST API
+/* The main reasons why to test API's:
+Couple explanation words:
+API tests are focused on Business logic - it means that they are veryfies functionality so are focusing on how something works not how something looks like
+== Reasons to test API:
+1. Functional tests - UNIT and INTEGRATION
+2. Much faster finds the bugs what eqals lesser cost of App development - more cases can be tested on API than on GUI
+3. Tests Backend for the following results:
+    * Efficiency
+    * Security/Safety
+    * Correction of received data
+=========== Benefits/Advantages of API Testing
+1. API Tests are much faster than GUI Tests
+2. Generates lesser cost and are easier in MAINTTENANCE
+3. Can Speeds up the GUI Tests
+4. Can be written and executed earlier than GUI tests
+5. Are inedependent of tech Stack an Language (including programming language)
+============ Disadvantages of API Testing:
+1. Sometimes can be more expensive (costs includes mostly Time of: documentation knowledge, tools needed to API testing, tests preparation (order is important))
+2. Can be harder to be implemented than GUI Tests
+3. Do not verifies interaction USER <--> GUI
+4. Sometimes not every functionality can be tested via API only
+*/
