5451547c0641b8de4e482002a7da37a7f97cae30
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-02 16:13:12 +0100

    What is GUI and UI added plus initial of What exactly API is added

---
+++ Practical_Part/3.GUI_Introduction/1.User_Interface.js
@@ -0,0 +1,20 @@
+// THIS PART IS DEDICATED FOR BASICS ABOUT USER INTERFACE and GUI (GRAQPHICAL USER INTERFACE)
+/* UI is used to communication between "user" and "device"
+UI  ensures:
+- Comfortable interaction
+- Standards
+- Specification - documentation/instruction for the user - is used for functional verification (As API is)
+========== UI what is connecting with
+1. With specific elemnts of device
+2. User do not need to know: how it works, the device building, software of device
+======== GUI what it is exactly
+It is graphical interface - shows what user sees on the device
+It includes:
+- Standards - buttons/displays/functions/etc.
+- Specification - describes more detailed functions
+========= GUI Additional information
+- Is used for Tests Pyramide
+- GUI Tests are very expensive during implementation and maintenance
+========== GUI Examples:
+- Part of Website which is needed to be tested - GUI do not need to be the whole website but can be separated to couple of parts
+*/
