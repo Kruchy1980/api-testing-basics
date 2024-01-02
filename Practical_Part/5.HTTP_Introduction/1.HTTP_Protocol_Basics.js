7ed8dabeb012f2e067ea0120122271055ca6a971
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-02 18:49:51 +0100

    The part of the HTTP Protocol and communicatio between client and server added

---
+++ Practical_Part/5.HTTP_Introduction/1.HTTP_Protocol_Basics.js
@@ -0,0 +1,27 @@
+// THAT PART IS DEDICATED TO HTTP PROTOCOL IN GENERAL
+/* === How to change the language in webbrowser - from the API:
+1.Go under URL: "chrome://settings/languages", add needed language clicking "Add language" button and than check the "Display Google Chrome in this language" in dropdown menu (3 dots icon on the right side of the language).
+=== How to reset DevTools
+1. Open debTools on the page
+2. Click the main gear - the top one if 2 are displaeyed
+3. Scroll down to the very bottom of the preferencess
+4. Press the "Restore defaults and reload" button.
+========== Practice:
+URL: "https://naukaapi.testoneo.com/1/"
+1. Open webBrowser and DevTools 
+2. Go to Network Tab
+3. Check theDisable Cache 
+4. Paste the URL from exercise + Enter
+What APIs were requested:
+Downloaded:
+1. Main page structure
+2. Logo - Stored at a different URL (quite long so I won't paste it)
+===The above displayed to the user
+3. 2 JS scripts that contain the code executed when you hover the cursor over the logo
+4. Full WebSite Address: "https://naukaapi.testoneo.com/1/"
+
+== Parts of URL Address:
+1. https:// --> protocol
+2. naukaapi.testoneo.com/ --> The URL for Server
+3. /1/ --> The path to proper resources
+*/
