08f6084ad6e0c6c32c6ce2468c37803135d0d2b5
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-04 14:39:47 +0100

    The Postman Basic Part Added

---
+++ Postman_Basics/Practical_Part/1.Add_and_send_Request.js
@@ -0,0 +1,33 @@
+// THIS PART IS DEDICATED TO BASICS OF REQUEST CREATION 
+/* To send request in postman proceed as following:
+URL: "https://naukaapi.testoneo.com/6/"
+1. Prepare request in postman for:
+    - Method GET
+    - Endpoints: "https://naukaapi.testoneo.com/api/users/", "https://naukaapi.testoneo.com/api/lessons/6/"
+    - No Other Headers
+==== RESULT Users:
+[
+    {
+        "id": 1,
+        "name": "Adam Nowak"
+    },
+    {
+        "id": 2,
+        "name": "Anna Kowalska"
+    },
+    {
+        "id": 3,
+        "name": "Jan Kowalski"
+    },
+    {
+        "id": 4,
+        "name": "Joanna Nowak"
+    },
+    {
+        "id": 5,
+        "name": "Jan Nowak"
+    }
+]
+=== RESULT Lesson
+{"title":"6. Dodatkowy front-end do API"}
+*/
