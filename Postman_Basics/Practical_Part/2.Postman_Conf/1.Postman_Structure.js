fba0f8d351815a34d30d6451bff170323f30479e
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-05 13:10:10 +0100

    Section 4 finished

---
+++ Postman_Basics/Practical_Part/2.Postman_Conf/1.Postman_Structure.js
@@ -0,0 +1,11 @@
+// THIS FILE IS DECICATED TO EXERCISE TRAINING - Description
+/* 1. Use Swagger API for retrive users data
+======1. Users List
+Method: GET
+Endpoint: "https://olive-nasal-game.glitch.me/api/users"
+##### TODO (All done in Postman Tool):
+1. Create new collection
+2. Create proper Requests for postman
+3. Save the request 
+========== 
+*/
