3ae9e4755d7e6ea46bc0b44372ba7189cca26f33
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-06 14:29:48 +0100

    Section 9 initialized plus section 8 updated

--- Postman_Basics/Practical_Part/6.PATCH_method/1.Record_Field_Update.js
+++ Postman_Basics/Practical_Part/6.PATCH_method/1.Record_Field_Update.js
@@ -36,4 +36,64 @@ pm.test("Id is less than 21", () => {
     console.log('Received response: ', jsonData.id);
     pm.expect(jsonData.id).to.below(21);
 });
+=================== Replacing methods and verifying their content in PATCH method - automatization
+#### Pre-request Script content
+//============================== First name updating
+let newFirstNameTemplate = '{{$randomFirstName}}';
+// To undederstand Postman variables let's display the timestam in console
+// console.log('Dynamic variable from postman: ', pm.variables.replaceIn(newFirstNameTemplate));
+// Now we can assign the expression to variable let's say
+newFirstName = pm.variables.replaceIn(newFirstNameTemplate)
+// Verifying
+// console.log('newFirstName: ', newFirstName);
+// Variable string
+pm.variables.set('firstName', newFirstName);
+// Verify
+console.log('firstName: ', pm.variables.get('firstName'));
+// ===================== Last name updating
+let newLastNameTemplate = '{{$randomLastName}}';
+// To undederstand Postman variables let's display the timestam in console
+console.log('Dynamic variable from postman: ', pm.variables.replaceIn(newLastNameTemplate));
+// Now we can assign the expression to variable let's say
+newLastName = pm.variables.replaceIn(newLastNameTemplate)
+// Verifying
+console.log('newLastName: ', newLastName);
+// Variable string
+pm.variables.set('lastName', newLastName);
+// Verify
+console.log('lastName: ', pm.variables.get('lastName'));
+
+#### Tests content
+//============ Variables for firstName and lastName declaration
+let firstName = pm.variables.get('firstName');
+console.log(firstName);
+let lastName = pm.variables.get('lastName');
+console.log(lastName);  
+// ======= Verify whether Id is lesser than 21
+pm.test("Id is less than 21", () => {
+    let jsonData = pm.response.json();
+    console.log('Received response: ', jsonData.id);
+    pm.expect(jsonData.id).to.below(21);
+});
+// ============== Verify First Name
+pm.test("Should the firstName be updated", () => {
+    let jsonData = pm.response.json();
+    console.log('Received response: ', jsonData.firstname);
+    pm.expect(jsonData.firstname).to.eql(firstName);
+});
+// =============== Verify Last Name
+pm.test("Should teh LastName be updated", () => {
+    let jsonData = pm.response.json();
+    console.log('Received response: ', jsonData.lastname);
+    pm.expect(jsonData.lastname).to.eql(lastName);
+});
+
+#### Request Body content
+{
+//   "email": "{{$randomExampleEmail}}",
+  "firstname": "{{firstName}}",
+  "lastname": "{{lastName}}",
+//   "password": "{{$randomBs}}",
+  "avatar": "https://cdn.pixabay.com/photo/2023/11/30/07/04/shetland-sheepdog-8420917_640.jpg"
+}
 */
