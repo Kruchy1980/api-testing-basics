02627e1448745a7db6f0efbb995696fde0af67a4
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-07 11:19:11 +0100

    The part of headers added

--- Postman_Basics/Practical_Part/8.Authorization_Postman/1.Excercise_Authorization.js
+++ Postman_Basics/Practical_Part/8.Authorization_Postman/1.Excercise_Authorization.js
@@ -1,1 +1,17 @@
 // THIS FILE CONTAINS THE EXERCISE FOR AUTHORIZATION IN POSTMAN
+/* Examples - Headers in Postman:
+========= 1.Create Article
+Method: POST
+Endpoint: https://olive-nasal-game.glitch.me/api/:resource --> Where :resource == articles
+# Request Body:
+{
+    "user_id": {{newId}},
+    "title": "{{$randomWords}}",
+    "body": "{{$randomLoremText}}",
+    "date": "{{$isoTimestamp}}",
+    "image": "https://cdn.pixabay.com/photo/2023/11/21/21/36/mountains-8404275_640.jpg"
+}
+=== Results in Headers tab of created to be verified in Postman - the most importantg are:
+Postman-Token: "8dcb77a2-c577-42bb-9532-7c1ccc18b1f6" <-- added automatically in Postman to make Authentication
+Accept: "* / *" <-- Here needed to add them with space it allows to display any format of files
+*/
