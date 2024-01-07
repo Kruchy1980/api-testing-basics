e990673ba3af86aa235f0fa830e4e3f80cd84c30
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-01-07 15:16:12 +0100

    The JWT Token Creation added

--- Postman_Basics/Practical_Part/8.Authorization_Postman/2.Basic_Auth.js
+++ Postman_Basics/Practical_Part/8.Authorization_Postman/2.Basic_Auth.js
@@ -45,5 +45,66 @@ pm.variables.set('myPass', mosiPass);
     - POST new Comment - Is done - add random user credentials
     - PATCH single comment - automate Auythorization
     - DELETE comment
+================ JWT_Authorization (For Service) - (JWT - Jason Web Token) =======================
+URL of SWAGGER used for this part: "https://olive-nasal-game.glitch.me/tools/swagger-v2.html"
+### Steps:
+1. New server adress
+2. Post/ register execution
+3. Copy Token
+4. Verification of Token under URL: "https://jwt.io" (Restrictions):
+    * !!! Do not use it out of the course
+    * Pay attention of validation time
+============= Token usage:
+1. Log in to SWAGGER
+2. Get All Plugins
+3. Verify results in Chrome DevTools:
+    * Header: Authorization: Barer[token]
+========================== Exercises:
+1. Open SWAGGER on Swagger(JWT)
+2. Expand first method :POST/register
+3. As body use Credentials of not real users eg:
+email: "Buddy_Emmerich11@example.org"
+password: "0iWpVbU9nS91b7l"
+-- Final Body Example:
+{
+  "email": "Buddy_Emmerich11@example.org",
+  "password": "0iWpVbU9nS91b7l"
+}
+=== RESULT:
+{
+  "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkJ1ZGR5X0VtbWVyaWNoMTFAZXhhbXBsZS5vcmciLCJwYXNzd29yZCI6IjBpV3BWYlU5blM5MWI3bCIsImlhdCI6MTcwNDYzMTc2MywiZXhwIjoxNzA0NjM1MzYzfQ.yObynqWfherd9dp1LKEqkHcL5w-bkY_NisKSEEpYBFI"
+}
+2. Copy the key only: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkJ1ZGR5X0VtbWVyaWNoMTFAZXhhbXBsZS5vcmciLCJwYXNzd29yZCI6IjBpV3BWYlU5blM5MWI3bCIsImlhdCI6MTcwNDYzMTc2MywiZXhwIjoxNzA0NjM1MzYzfQ.yObynqWfherd9dp1LKEqkHcL5w-bkY_NisKSEEpYBFI
+3. Use the key in Swagger in GET/plugin request
+4. Press the lock icon
+5. Paste our key and press Authorize
+6. Close the popup
+======== Decode the token !!!! use only for course purpose never on production
+1. Go under URL: "https://jwt.io"
+2. Paste our code
+3. Verify the displayed Data
+### !!!! Pay attention that our token includes 3 parts separate by '.':
+1. eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9 --> describersd ALGOITHM (used) and TOKEN TYPE
+{
+  "alg": "HS256",  <-- Algorithm used for encoding token
+  "typ": "JWT" <-- Type of Token
+}
+2. eyJlbWFpbCI6IkJ1ZGR5X0VtbWVyaWNoMTFAZXhhbXBsZS5vcmciLCJwYXNzd29yZCI6IjBpV3BWYlU5blM5MWI3bCIsImlhdCI6MTcwNDYzMTc2MywiZXhwIjoxNzA0NjM1MzYzfQ <-- Payload sent by API
+{
+  "email": "Buddy_Emmerich11@example.org", <-- User Email (!!!! Newraulic data)
+  "password": "0iWpVbU9nS91b7l", <-- user password (!!!! Very dangerous data if not encoded before display)
+  "iat": 1704631763, <-- when the token was created
+  "exp": 1704635363 <-- What is the token expiration time
+}
+!!! To verify the times go under URL:  "https://epochconverter.com" and paste the times
+3. yObynqWfherd9dp1LKEqkHcL5w-bkY_NisKSEEpYBFI <-- Signature verification
+HMACSHA256(
+  base64UrlEncode(header) + "." +
+  base64UrlEncode(payload),
+  your-256-bit-secret
+) secret base64 encoded
+// =============== Creating CRUD for Admin Resources
+===== 1. Creation of random user Token
+All in postman collection added - no time for copy paste here - To be done later
 
 */
